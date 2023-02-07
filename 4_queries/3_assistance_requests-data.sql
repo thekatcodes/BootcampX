500be1dae0d022ab50ce73ac61ab65bbd1f0dd6f
Katie Liu <katieliu59@gmail.com>
2023-02-07 15:04:37 -0500

    Get important data about each assistance request

---
+++ 4_queries/3_assistance_requests-data.sql
@@ -0,0 +1,11 @@
+SELECT
+    teachers.name AS teacher,
+    students.name AS student,
+    assignments.name AS assignment,
+    (completed_at-started_at) as duration
+
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
