2a7cd6c21afbf8a07eec6bf27919aec8e31dddd0
Katie Liu <katieliu59@gmail.com>
2023-02-08 23:42:52 -0500

    Update students.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,17 +6,22 @@ const pool = new Pool({
 	host: "localhost",
 	database: "bootcampx",
 });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
     res.rows.forEach(user => {
-        console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`); //Armand Hilll has an id of 1 and was in the 1 cohort
+        console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`); //Armand Hilll has an id of 1 and was in the 1 cohort
         console.log(user); //{ id: 1, name: 'Armand Hilll', cohort_id: 1 }
   });
 })
